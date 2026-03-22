ceea7d3b0d72dc23fd320ed59b89b27855e0f1e0
Adhil-cr <m.adhilcr7@gmail.com>
2026-03-23 00:20:20 +0530

    Remove password reset flow

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include, reverse_lazy
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -38,37 +37,4 @@ urlpatterns = [
     path('seating/view/', views.seating_view_page, name="seating_view"),
     path('profile/', views.profile_page, name="profile"),
 
-    # Password reset
-    path(
-        "password-reset/",
-        auth_views.PasswordResetView.as_view(
-            template_name="registration/password_reset_form.html",
-            email_template_name="registration/password_reset_email.html",
-            subject_template_name="registration/password_reset_subject.txt",
-            success_url=reverse_lazy("password_reset_done"),
-        ),
-        name="password_reset",
-    ),
-    path(
-        "password-reset/done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="registration/password_reset_done.html"
-        ),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name="registration/password_reset_confirm.html",
-            success_url=reverse_lazy("password_reset_complete"),
-        ),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="registration/password_reset_complete.html"
-        ),
-        name="password_reset_complete",
-    ),
 ]
